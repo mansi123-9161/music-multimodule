18770eeaffade134513cc987de3393ce72b57fbd
mansi123-9161 <mansichourasia100@gmail.com>
2019-08-09 12:00:17 +0530

    updated multimodule task

---
+++ README.md
@@ -0,0 +1,8 @@
+ # Muzix-Player-Multi-Module
+ # Added all the modules
+ # 1.track-service
+ # 2.user-service
+ # 3.api-gateway
+ # 4.config-server
+ # 5.eureka-server
+ 
---
+++ api-gateway/src/main/java/com/stackroute/microservice/ApiGatewayPattern.java
@@ -0,0 +1,21 @@
+package com.stackroute.microservice;
+
+import com.stackroute.microservice.filters.ErrorFilter;
+import com.stackroute.microservice.filters.PostFilter;
+import com.stackroute.microservice.filters.PreFilter;
+import com.stackroute.microservice.filters.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayPattern {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiGatewayPattern.class, args);
+	}
+
+}
